6125a6382a6a69863acd1789b1e3353f5ff2e14b
AndrezzaSantos <andrezza.saantoos@gmail.com>
2022-04-18 16:28:19 -0300

    New page home

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:dosagemdecloro/calculadora.dart';
+import 'package:dosagemdecloro/home.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Calculadora(),
+    //home: Calculadora(),
+    home: Home(),
   ));
 }
 
