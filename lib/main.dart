3e3f35283dd9874bdf51665d06eae8f1da4affe8
AndrezzaSantos <andrezza.saantoos@gmail.com>
2022-04-18 22:14:41 -0300

    Merge branch 'master' of https://github.com/AndrezzaSantos/Calculadora_Dosagem_Cloro

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-//import 'package:dosagemdecloro/calculadora.dart';
+import 'package:dosagemdecloro/calculadora.dart';
 import 'package:dosagemdecloro/home.dart';
 import 'package:flutter/material.dart';
 
